4f8b63043b86130325cad0367594e8f79ebb6370
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-06-16 23:31:21 -0700

    update: added a few imports

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,18 @@
-// utils/redis.js
-
 const redis = require('redis');
+const util = require('util');
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.client.on('error', (err) => {
-      console.error(`Redis Client Error: ${err}`);
+    this.getAsync = util.promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.error(`Redis client error: ${error.message}`);
+    });
+
+    this.client.on('connect', () => {
+      // Optional: Log successful connection
+      console.log('Redis client connected to the server');
     });
   }
 
@@ -15,39 +21,29 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
+    try {
+      const value = await this.getAsync(key);
+      return value;
+    } catch (error) {
+      console.error(`Failed to retrieve value for key '${key}': ${error.message}`);
+      return null; // Return null or handle the error as needed
+    }
   }
 
   async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', duration, (err, reply) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+    try {
+      this.client.setex(key, duration, value);
+    } catch (error) {
+      console.error(`Failed to set value for key '${key}': ${error.message}`);
+    }
   }
 
   async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (err, reply) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+    try {
+      this.client.del(key);
+    } catch (error) {
+      console.error(`Failed to delete key '${key}': ${error.message}`);
+    }
   }
 }
 
