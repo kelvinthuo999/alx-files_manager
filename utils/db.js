2035cf37e3d28b7fa2bb53f5f70f7a93c5af20bf
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-06-17 22:54:41 -0700

    create: script to connect to mongod

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+// utils/db.js
+
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(database);
+    }).catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client && this.client.topology && this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+// Create and export an instance of DBClient
+const dbClient = new DBClient();
+export default dbClient;
