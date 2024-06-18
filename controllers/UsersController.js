1db64d8c964fe6c34478071b4012bed3f144b2e6
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-06-18 05:14:51 -0700

    try: handling advanced tasks

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,26 @@
 // controllers/UsersController.js
 
-import { redisClient } from '../utils/redis';
+import dbClient from '../utils/db';
+import userQueue from '../utils/worker'; // Import the userQueue from worker.js
+
+async function postNew(email, password) {
+  try {
+    // Insert new user into DB
+    const result = await dbClient.client.db().collection('users').insertOne({ email, password });
+
+    const newUser = {
+      id: result.insertedId,
+      email,
+    };
+
+    // Enqueue a job to send welcome email to the new user
+    await userQueue.add({ userId: newUser.id });
+
+    return newUser;
+  } catch (error) {
+    throw new Error('Unable to create user');
+  }
+}
 
 async function getMe(token) {
   const key = `auth_${token}`;
@@ -27,5 +47,6 @@ async function getMe(token) {
 }
 
 export default {
+  postNew,
   getMe,
 };
